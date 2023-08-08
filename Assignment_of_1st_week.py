788371af18331e3d15a20f3d1298ea1a3bc368b5
codewithgagangandhi <141737458+codewithgagangandhi@users.noreply.github.com>
2023-08-08 18:06:22 +0530

    Add files via upload

---
+++ Assignment_of_1st_week.py
@@ -0,0 +1,44 @@
+# Q1. Create one variable containing following type of data:
+# (i)	string
+# (ii)	list
+# (iii)	float
+# (iv)	tuple
+gagan="gagan"
+l=[2.2,3,4,56,"gagan"]
+flo=3.2
+tup=(2,3,4,45,"gqagan")
+print(type(gagan))
+print(type(l))
+print(type(flo))
+print(type(tup))
+# Q2. Given are some following variables containing data:
+# (i)	var1 = ‘ ‘
+# (ii)	var2 = ‘[ DS , ML , Python]’
+# (iii)	var3 = [ ‘DS’ , ’ML’ , ‘Python’ ]
+# (iv)	var4 = 1.
+# What will be the data type of the above given variable
+var1=''
+print(type(var1))# string 
+var2='[ds,ml,python]'
+print(type(var2))# string
+# 3 ka list hoga 
+# 4 ka int hoga 
+# Q3. Explain the use of the following operators using an example:
+
+# (i)	/ - division which gives float answer 
+# (ii)	% - modulus 
+# (iii)	//  floor divide - which gives int value 
+# (iv)	** - power 
+print(4/2)
+# Q4. Create a list of length 10 of your choice containing multiple types of data. Using for loop print the 
+# element and its data type
+l=[2,3,4,5,5.6,"gagan",7.5,90,9.8,'hagan']
+n=10
+i=1
+for i in l:
+    print(i)
+    print(type(i))
+
+# Q5. Using a while loop, verify if the number A is purely divisible by number B and if so then how many 
+# times it can be divisible  
+
